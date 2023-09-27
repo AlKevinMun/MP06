601c359ed515144d34afabf9b54f90e029f88b26
Kevin <amunozgil@elpuig.xeill.net>
2023-09-27 18:38:47 +0200

    Nuevos exemplos

---
+++ UF1/src/src/ejemplos/OutputBytes.java
@@ -0,0 +1,22 @@
+package ejemplos;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class OutputBytes {
+
+    public static void main(String[] args) {
+        int data[] = {65, 66, 67, 68, 69};
+
+        try {
+            FileOutputStream fos = new FileOutputStream("out/production/MP06/ejemplos/Main.byte");
+            for (int i = 0; i < data.length; i++) {
+                fos.write(data[i]);
+            }
+            fos.close();
+        } catch(IOException e) {
+            System.out.println("ERROR");
+            e.printStackTrace();
+        }
+    }
+}
--- UF1/src/src/ejercicios/CSVToScreen.java
+++ UF1/src/src/ejercicios/CSVToScreen.java
@@ -1,12 +1,39 @@
 package ejercicios;
+
 import java.io.*;
+import java.util.Scanner;
 
 public class CSVToScreen {
-    public static void main(String[] args) {
-        try{
+    public static void main(String[] args) throws FileNotFoundException {
+        BufferedReader inputStream = new BufferedReader(new FileReader(new File("users.csv")));
 
-        } catch (IOException e){
+        try {
+            String line = "";
+            int i = 1;
+            while ((line = inputStream.readLine()) != null) {
+                String[] parts = line.split(",");
 
+                System.out.print("Persona "+i);
+                System.out.println();
+                System.out.print("firstname: "+parts[0]);
+                System.out.println();
+                System.out.print("lastname: "+parts[1]);
+                System.out.println();
+                System.out.print("username: "+parts[2]);
+                System.out.println();
+                System.out.print("password: "+parts[3]);
+                System.out.println();
+                System.out.println("email: "+parts[4]);
+                System.out.println("****");
+
+                i++;
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Fichero no encontrado");
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
